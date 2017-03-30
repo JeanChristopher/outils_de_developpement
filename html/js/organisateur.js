aca2d4506687772a61b15999547e9e217bf6d692
gui <herault.guillaume@hotmail.fr>
2017-03-30 14:01:49 +0200

    maj fonction BDD

--- html/js/organisateur.js
+++ html/js/organisateur.js
@@ -8,6 +8,7 @@ projectList = document.getElementById('Projets');
 
 function fillProjectList(projectList) {
 
+
 for(var i = 0; i < arrayOfProjects.length; i++)
 	//Add div for materialize.css
 	
@@ -21,7 +22,7 @@ for(var i = 0; i < arrayOfProjects.length; i++)
 	
 	var materialIcons  = document.createElement('i');
 	materialIcons.setAttribute("class","material-icons");
-	materialIcons.value = "filter_drama"
+	materialIcons.value = "event_note"
 	
 	var collapsBody = document.createElement('div');
 	collapsBody.setAttribute("class","collapsible-body")
@@ -31,30 +32,42 @@ for(var i = 0; i < arrayOfProjects.length; i++)
     for(var j = 0; j < (arrayOfProjects.lenght-1); j++) {
         // Create the list item:
         var person = document.createElement('li');
-
+        
+        var input = document.createElement('input');
+        input.setAttribute("type","checkbox");
+        input.setAttribute("class","filled-in");
+        input.setAttribute("id","A DEFINIR");
+       
+		var label = document.createElement('label');
+		input.setAttribute("for","A DEFINIR");
+		
+		
+		
 		//Mettre les variables qui manquent
 		
+		<input type="checkbox" class="filled-in" id="filled-in-box1"/><label for="filled-in-box1">Personne 1</label>
         // Set its contents:
         person.value = arrayOfProjects[i][j];
 
         // Add it to the list:
         listPeople.appendChild(person);
     }
+    
 	collapsBody.appendChild(listPeople)
 	
+	collapsHeader.appendChild(materialIcons)
 	collapsHeader.appendChild(collapsBody)	
-    collapsHeader.appendChild(collapsBody)
-    
-	
 
-    // Set its contents:
-    item.appendChild(document.createTextNode(array[i]));
-
-    // Add it to the list:
-    list.appendChild(item);
 }
 
-function removeProjectFromList(projectList) {
+function createIdPerson() {
+}
+
+function addProject(projectList) {
+
+}	
+	
+function removeProject(projectList) {
 
 }
 
