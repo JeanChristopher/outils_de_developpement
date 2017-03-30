e8a6ccf429b583998f82a15b0691668792642d60
gui <herault.guillaume@hotmail.fr>
2017-03-30 14:58:15 +0200

    Adaptation au tableau BDD

--- html/js/organisateur.js
+++ html/js/organisateur.js
@@ -1,66 +1,67 @@
+arrayOfProjects = [ ['0','Gui','0','Chat'],['0','Gui','1','Chat'],['0','Gui','2','Chat'],['1','M','0','Chat'] ];
 
-
-arrayOfProjects = [ ['nomProjet','nomPersonne1', 'nomPersonne1','nomPersonne1','nomPersonne1' ] ]
-// Add the contents of options[0] to #foo: 
+function fillProjectList() {
 
 projectList = document.getElementById('Projets');
 
-
-function fillProjectList(projectList) {
-
-
 for(var i = 0; i < arrayOfProjects.length; i++)
-	//Add div for materialize.css
-	
-	var collapsHeader = document.createElement('div');
-	collapsHeader.setAttribute("class","collapsible-header");
-	collapsHeader.value = projectList[i][0] 
-	
-	/*var newbadge  = document.createElement('span');
-	newbadge.setAttribute("class","new badge");
-	newbadge.value = "4"*/
-	
-	var materialIcons  = document.createElement('i');
-	materialIcons.setAttribute("class","material-icons");
-	materialIcons.value = "event_note"
-	
-	var collapsBody = document.createElement('div');
-	collapsBody.setAttribute("class","collapsible-body")
-	
-	var listPeople = document.createElement('ul');
 
-    for(var j = 0; j < (arrayOfProjects.lenght-1); j++) {
-        // Create the list item:
-        var person = document.createElement('li');
-        
-        var input = document.createElement('input');
-        input.setAttribute("type","checkbox");
-        input.setAttribute("class","filled-in");
-        input.setAttribute("id","A DEFINIR");
-       
-		var label = document.createElement('label');
-		input.setAttribute("for","A DEFINIR");
+	//Si le projet n'exite pas, on le crée et on rajoute les divs de materialize.css
+	if (!document.getElementById(projectList[i][0])) {
 		
-		
-		
-		//Mettre les variables qui manquent
-		
-		<input type="checkbox" class="filled-in" id="filled-in-box1"/><label for="filled-in-box1">Personne 1</label>
-        // Set its contents:
-        person.value = arrayOfProjects[i][j];
-
-        // Add it to the list:
-        listPeople.appendChild(person);
-    }
+	    var project = document.createElement('li');
+	    project.setAttribute("id",""+arrayOfProjects[i][0]);
+	    
+	    var collapsHeader = document.createElement('div');
+	    collapsHeader.setAttribute("class","collapsible-header");
+	    collapsHeader.value = projectList[i][0];
+	    
+	    /*var newbadge  = document.createElement('span');
+	    newbadge.setAttribute("class","new badge");
+	    newbadge.value = "4"*/
+	    
+	    var materialIcons  = document.createElement('i');
+	    materialIcons.setAttribute("class","material-icons");
+	    materialIcons.value = "event_note";
+	    
+	    var collapsBody = document.createElement('div');
+	    collapsBody.setAttribute("class","collapsible-body");
+	    
+	    var listPeople = document.createElement('ul');
+	    
+	    collapsBody.appendChild(listPeople);
+		collapsHeader.appendChild(materialIcons);
+		collapsHeader.appendChild(collapsBody);	
+	}
+	else {
+		// Sinon ca existe on recupere uniquement listPeople
+		var project = document.getElementById(projectList[i][0]);
+		var listPeople = project.getElementsByTagName("ul");
+	
+	}
+    // add person:
+    var person = document.createElement('li');
     
-	collapsBody.appendChild(listPeople)
+    var input = document.createElement('input');
+    input.setAttribute("type","checkbox");
+    input.setAttribute("class","filled-in");
+    input.setAttribute("id",""+projectList[i][2]);
+    
+	var label = document.createElement('label');
+	input.setAttribute("for",""+projectList[i][2]);
 	
-	collapsHeader.appendChild(materialIcons)
-	collapsHeader.appendChild(collapsBody)	
+    // Set its contents:
+    label.value = arrayOfProjects[i][2];
 
+    // Add it to the list:
+    person.appendChild(input);
+    person.appendChild(label);
+    listPeople.appendChild(person);
+    
 }
 
 function createIdPerson() {
+	
 }
 
 function addProject(projectList) {
@@ -80,4 +81,4 @@ function removePerson() {
 }
 
 
-
+fillProjectList();
