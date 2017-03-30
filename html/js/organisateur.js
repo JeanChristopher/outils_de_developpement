28ec68a01ea8172de310a0d70f3b75ad0988fd85
gui <herault.guillaume@hotmail.fr>
2017-03-30 21:00:03 +0200

    Ajout de commentaire dans le js

--- html/js/organisateur.js
+++ html/js/organisateur.js
@@ -2,87 +2,84 @@
 
 function fillProjectList() {
 	
+	// tableau de test
     arrayOfProjects = [ 
     					['0','Projet1','0','Marion'],
     					['0','Projet1','1','Laurent'],
     					['0','Projet1','2','Aiman'],
     					['1','Projet2','3','Jean-Chris'] 
     				];
-    				
+    // On récupère l'objet liste qui contient tout les futurs projets				
     projectList = document.getElementById('Projets');
     
+    // boucle sur le tableau de la bdd
     for(var i = 0; i < arrayOfProjects.length; i++) {
-    console.log(document.getElementById("idProjet"+arrayOfProjects[i][0]))
-    	//Si le projet n'exite pas, on le crée et on rajoute les divs de materialize.css
+		
+    	//Si le projet n'exite pas
     	if (!document.getElementById("idProjet"+arrayOfProjects[i][0])) {
 			
-    		console.log("creation")
+			// On créé un élément de la liste = un projet
     	    var project = document.createElement('li');
+    	    // On lui donne le même id que celui de la bdd
     	    project.setAttribute("id","idProjet"+arrayOfProjects[i][0]);
     	    
+    	    // Div materialize css
     	    var collapsHeader = document.createElement('div');
     	    collapsHeader.setAttribute("class","collapsible-header");
     	    collapsHeader.textContent = arrayOfProjects[i][1];
     	    
-    	    /*var newbadge  = document.createElement('span');
-    	    newbadge.setAttribute("class","new badge");
-    	    newbadge.value = "4"*/
-    	    
+			// Div materialize css
     	    var materialIcons  = document.createElement('i');
     	    materialIcons.setAttribute("class","material-icons");
     	    materialIcons.textContent = "event_note";
-    	    
+			// Div materialize css    	    
     	    var collapsBody = document.createElement('div');
     	    collapsBody.setAttribute("class","collapsible-body");
-    	    
-    	    var listPeople = document.createElement('ul');
-			listPeople.setAttribute("class","test");
 			
+			// Créé la liste des personnes qui vont participer au projet    	    
+    	    var listPeople = document.createElement('ul');
+			listPeople.setAttribute("class","listPeople");
+			
+			// Créé un formulaire pour exploiter les tick et le php (encore à définir)
 			var form = document.createElement("FORM");
-			 	    
+			
+			// On ajoute chaque noeud à son parent
     	    form.appendChild(listPeople);
     	    collapsBody.appendChild(form);
     		collapsHeader.appendChild(materialIcons);
     		project.appendChild(collapsHeader);
     		project.appendChild(collapsBody);
-
-    		projectList.appendChild(project);
-    		console.log("fin_creation")
+			projectList.appendChild(project);
     	}
     	
 
-    	// Sinon ca existe on recupere les variables sur lesquelles on bosse
+    	// On récupère les variables sur lequels on travaille
     	var project = document.getElementById("idProjet"+arrayOfProjects[i][0]);
-    	var listPeople = project.getElementsByClassName("test")[0];
+    	var listPeople = project.getElementsByClassName("listPeople")[0];
     	var collapsBody = project.getElementsByClassName("collapsible-body")[0];
     	var form = project.getElementsByTagName("FORM")[0];  	
     	
-        // add person
+        // Crée un nouvel élément de liste, correspond à une personne
         var person = document.createElement('li');
+        
+        // Créé un élément checkbox pour supprimer ultérieurement une personne
         var input = document.createElement('input');
         input.setAttribute("type","checkbox");
         input.setAttribute("class","filled-in");
         input.setAttribute("id","idPerson"+arrayOfProjects[i][2]);
         
+        //Créé un élément label obligatoire pour aller avec l'input
     	var label = document.createElement('label');
     	label.setAttribute("for","idPerson"+arrayOfProjects[i][2]);
     	
-        // Set its contents:
+        // Ajoute le nom au label:
         label.textContent = arrayOfProjects[i][3];
-		console.log(label.textContent)
 		
-        // Add it to the list:
-        
+        // Ajoute chaque noeud à son parent 
         person.appendChild(input);
         person.appendChild(label);
-        
         listPeople.appendChild(person);
-        
         form.appendChild(listPeople);
-        
-        
-        
-        console.log('fin')
     
 	}
 }
