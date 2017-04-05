4e2c1c4fa520e3e30b9adee249da59c9b3094ad8
LaurentBAZILE <laurent.bazile@outlook.fr>
2017-04-05 12:10:26 +0200

    Add files via upload
    mise à jour ajax et php

---
+++ organisateur.js
@@ -0,0 +1,140 @@
+
+
+function fillProjectList() {
+	
+	// tableau de test
+    arrayOfProjects = [ 
+    					['0','Projet1','0','Marion'],
+    					['0','Projet1','1','Laurent'],
+    					['0','Projet1','2','Aiman'],
+    					['1','Projet2','3','Jean-Chris'] 
+    				];
+    // On récupère l'objet liste qui contient tout les futurs projets				
+    projectList = document.getElementById('Projets');
+    
+    // boucle sur le tableau de la bdd
+    for(var i = 0; i < arrayOfProjects.length; i++) {
+		
+    	//Si le projet n'exite pas
+    	if (!document.getElementById("idProjet"+arrayOfProjects[i][0])) {
+			
+			// On créé un élément de la liste = un projet
+    	    var project = document.createElement('li');
+    	    // On lui donne le même id que celui de la bdd avec le prefix idProjet
+    	    project.setAttribute("id","idProjet"+arrayOfProjects[i][0]);
+    	    
+    	    // Div materialize css
+    	    var collapsHeader = document.createElement('div');
+    	    collapsHeader.setAttribute("class","collapsible-header");
+    	    collapsHeader.textContent = arrayOfProjects[i][1];
+    	    
+			// Div materialize css
+    	    var materialIcons  = document.createElement('i');
+    	    materialIcons.setAttribute("class","material-icons");
+    	    materialIcons.textContent = "event_note";
+			// Div materialize css    	    
+    	    var collapsBody = document.createElement('div');
+    	    collapsBody.setAttribute("class","collapsible-body");
+			
+			// Créé la liste des personnes qui vont participer au projet    	    
+    	    var listPeople = document.createElement('ul');
+			listPeople.setAttribute("class","listPeople");
+			
+			// Créé un formulaire pour exploiter les tick et le php (encore à définir)
+			var form = document.createElement("FORM");
+			
+			// On ajoute chaque noeud à son parent
+    	    form.appendChild(listPeople);
+    	    collapsBody.appendChild(form);
+    		collapsHeader.appendChild(materialIcons);
+    		project.appendChild(collapsHeader);
+    		project.appendChild(collapsBody);
+			projectList.appendChild(project);
+    	}
+    	
+
+    	// On récupère les variables sur lequels on travaille
+    	var project = document.getElementById("idProjet"+arrayOfProjects[i][0]);
+    	var listPeople = project.getElementsByClassName("listPeople")[0];
+    	var collapsBody = project.getElementsByClassName("collapsible-body")[0];
+    	var form = project.getElementsByTagName("FORM")[0];  	
+    	
+        // Crée un nouvel élément de liste, correspond à une personne
+        var person = document.createElement('li');
+        
+        // Créé un élément checkbox pour supprimer ultérieurement une personne
+        var input = document.createElement('input');
+        input.setAttribute("type","checkbox");
+        input.setAttribute("class","filled-in");
+        input.setAttribute("id","idPerson"+arrayOfProjects[i][2]);
+        
+        //Créé un élément label obligatoire pour aller avec l'input
+    	var label = document.createElement('label');
+    	label.setAttribute("for","idPerson"+arrayOfProjects[i][2]);
+    	
+        // Ajoute le nom au label:
+        label.textContent = arrayOfProjects[i][3];
+		
+        // Ajoute chaque noeud à son parent 
+        person.appendChild(input);
+        person.appendChild(label);
+        listPeople.appendChild(person);
+        form.appendChild(listPeople);
+    
+	}
+}
+//Ajouter un projet
+var avoir_valeur = document.getElementById('nouv_projet');
+avoir_valeur.addEventListener('submit', addProject);
+var saisie = avoir_valeur.elements["new_projet"] ;
+
+
+function addProject(projectList) {
+    
+    var ajax = new XMLHttpRequest();
+	//projectList.preventDefault();
+    
+    ajax.open('POST', 'php/requete.php', true);
+
+    ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    ajax.addEventListener('readystatechange',  function(e) {
+        if(ajax.readyState == 4 && ajax.status == 200) {
+//            document.getElementById('retour').innerHTML= ajax.responseText;    
+        }        
+    });    
+    
+    var data = "projet="+saisie.value;
+    ajax.send(data);
+    
+    
+    projectList.preventDefault();
+}
+
+
+	
+	
+/*function removeProject(projectList) {
+	// On récupère l'objet liste qui contient tout les futurs projets				
+    projectList = document.getElementById('Projets');
+    
+    // Trouver le projet actif
+    element = projectList.removeChild(projectList.querySelector('[class="active"]'));
+    
+    // Ne pas oublier de mettre a jour la bdd
+}
+
+function addPerson() {
+
+}
+
+function removePerson() {
+
+}
+
+function setListeners {
+	//document.addEventListener
+}
+*/
+//fillProjectList();
+
---
+++ requete.php
@@ -0,0 +1,57 @@
+<?php
+//connexion au serveur
+$link = pg_connect ("host=localhost port=5432 dbname=Application user=postgres password=postgres");
+
+if(!$link){
+  die ("Erreur de conexion");
+}
+  else{
+    echo "Succès de connexion";
+  }
+
+
+
+
+//TABLE PROJET
+
+//insertion d'une ligne dans la table projet 
+
+$projet = $_POST['projet'];
+
+$requeteInsert = "INSERT INTO projet(nom,importance,commentaires) VALUES('$projet', '', '')";
+pg_query($link, $requeteInsert);
+/*
+// suppression d'une ligne dans la table 
+$requeteDelete = "DELETE FROM projet WHERE nom like 'fkkf' ";
+pg_query($link,$requeteDelete);
+*/
+
+//TABLE UTILISATEURS
+
+//Afficher tous les utilisateurs
+/*
+$requete = "SELECT id, nom, prenom, FROM employe";
+
+if($result = pg_query($link, $requete)) {
+            while($ligne = pg_fetch_assoc($result)) {
+                $nom1 = $ligne["nom"];
+                $prenom1 = $ligne["prenom"];
+            
+                echo "<table><tr><td> ".$nom1." ".$prenom1."</table></tr></td>" ;
+                
+                //"<option value='" .$id. "'>" .$employe. "</option>" 
+                                   
+            }
+        }
+
+
+// TABLE PROJETS ET UTILISATEURS
+
+$requeteInsert = "INSERT INTO Projet_en_cours
+                  VALUES(id_projet, 'nomprojet', 'id_employe', 'nom_employe')";
+
+
+
+pg_query($link,$requeteInsert);
+*/
+?>
