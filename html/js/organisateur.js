91ba2558f42e62a18d168d132e7f0abf3e28025f
gui <herault.guillaume@hotmail.fr>
2017-04-10 18:13:48 +0200

    Correction bug presentation checkbox qui ne marchait pas

--- html/js/organisateur.js
+++ html/js/organisateur.js
@@ -6,6 +6,7 @@ function fillProjectList() {
     var features;
     var nom;
     
+    //Recupere la table projet en cours de la bdd
     var ajax1 = new XMLHttpRequest();
 	ajax1.open('POST', 'php/requete.php', true);
 	ajax1.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -15,12 +16,10 @@ function fillProjectList() {
             var option = document.getElementById("selection")
             
             for (var i = 0; i < features.length; i++ ){
-				  arrayOfProjects.push([features[i].id_projet,features[i].nom_projet,features[i].id_employe,features[i].nom_employe,features[i].prenom_employe]);
-                  
-                    
+				  arrayOfProjects.push([features[i].id_projet,features[i].nom_projet,features[i].id_employe,features[i].nom_employe,features[i].prenom_employe]);       
             }
             
-             // On récupère l'objet liste qui contient tout les futurs projets				
+    // On récupère l'objet liste du dom qui contient tout les futurs projets				
     projectList = document.getElementById('Projets');
     
     // boucle sur le tableau de la bdd
@@ -77,12 +76,12 @@ function fillProjectList() {
         var input = document.createElement('input');
         input.setAttribute("type","checkbox");
         input.setAttribute("class","filled-in");
-        input.setAttribute("id","idPersonne"+arrayOfProjects[i][2]);
+        input.setAttribute("id",idProject+"idPersonne"+arrayOfProjects[i][2]);
         input.addEventListener("click",activDesactivBoutonSupprPerso);
         
         //Créé un élément label obligatoire pour aller avec l'input
     	var label = document.createElement('label');
-    	label.setAttribute("for","idPersonne"+arrayOfProjects[i][2]);
+    	label.setAttribute("for",idProject+"idPersonne"+arrayOfProjects[i][2]);
     	
         // Ajoute le nom au label:
         label.textContent = arrayOfProjects[i][3]+" "+arrayOfProjects[i][4];
@@ -106,7 +105,7 @@ function fillProjectList() {
 	
 }
 
-// Ajoute un projet dans  la liste des projets
+// Ajoute un projet dans la liste des projets
 function addProject() {
 				
 	//    Il y a 3 requetes ajax imbriquées :
@@ -206,13 +205,13 @@ function addProject() {
   
         var data = "idFonction=checkIfProjectExists&nomProjet="+nomProjet;
         ajax2.send(data);
+        
+        //Disable le bouton projet puisque le champs est vide
         var elmtNomNewProjet = document.getElementById("nom_new_projet");
 		elmtNomNewProjet.value = "";
 		elmtNomNewProjet.className="validate";
 		var elmtBtnAjoutPerso = document.getElementById("BtnAjoutPerso");
-		//elmtBtnAjoutPerso.className = "waves-effect waves-light btn disabled";
         
-        // !!(opt) Vider le champs
 }
 
 //Retire un projet
@@ -236,6 +235,7 @@ function removeProject() {
 		}        
 	});
 	
+	//Disable le bouton supprimer projet puisque qu'aucun projet n'est selectionné
 	var data = "idFonction=removeProject&idProject="+idProject;
 	ajax1.send(data);
 	var elmtBtnSuppProjet = document.getElementById("BtnSuppProjet");
@@ -289,7 +289,8 @@ function addPersonne() {
 	// On récupère l'objet liste qui contient tout les futurs projets				
     var projectList = document.getElementById('Projets');
     // Trouver le projet actif
-    var nomProjet = projectList.querySelector('[class="active"]');    
+    var nomProjet = projectList.querySelector('[class="active"]');
+        
     //Recupere l'id contenue dans le noeud
     var idProject = nomProjet.id.replace('idProjet','');
     
@@ -298,10 +299,7 @@ function addPersonne() {
     //Recupere l'id contenue dans le noeud
 	var idPersonne = e.options[e.selectedIndex].value.replace('idPersonne','');
 	
-    //Vérifie si la personne n'est pas déjà dans le projet
-	//if (!nomProjet.childNodes.indexOf(idPersonne) > -1) {	
-	//} 
-	// -> Regarder si on ne peut pas retirer tout les éléments présents de la liste des choix
+	// -> !!! (opt) Regarder si on ne peut pas retirer tout les éléments présents de la liste des choix
 	
     // On créé un élément de la liste = un projet
     console.log("test pour vérifier pourquoi creation de n personne parfois...")
@@ -311,12 +309,12 @@ function addPersonne() {
     var input = document.createElement('input');
     input.setAttribute("type","checkbox");
     input.setAttribute("class","filled-in");
-    input.setAttribute("id","idPersonne"+idPersonne);
+    input.setAttribute("id",idProject+"idPersonne"+idPersonne);
     input.addEventListener("click",activDesactivBoutonSupprPerso);
     
     //Créé un élément label obligatoire pour aller avec l'input
     var label = document.createElement('label');
-    label.setAttribute("for","idPersonne"+idPersonne);
+    label.setAttribute("for",idProject+"idPersonne"+idPersonne);
     
     // Ajoute le nom au label:
     label.textContent = e.options[e.selectedIndex].text;
@@ -327,6 +325,7 @@ function addPersonne() {
     personne.appendChild(label);
     listPeople.appendChild(personne);
     
+    //Ajoute une personne dans un projet en cours dans la bdd
     var ajax1 = new XMLHttpRequest();
 	ajax1.open('POST', 'php/requete.php', true);
 	ajax1.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -353,8 +352,7 @@ function removePersonne() {
 
 	var projetToRemove = '(';
 	for (var i = 0; i < checkedBoxes.length; i++){
-		//checkedBoxes[i].value.replace('idPersonne','');
-		projetToRemove = projetToRemove + String(checkedBoxes[i].getAttribute("id").replace('idPersonne','')+",");
+		projetToRemove = projetToRemove + String(checkedBoxes[i].getAttribute("id").replace(/\d+idPersonne/,'')+",");
 	}
 	//Retire la derniere virgule
 	projetToRemove = projetToRemove.slice(0,-1) + ')'
